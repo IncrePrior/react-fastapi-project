3edabc76c59f275e28419ec792b25ed8fd17c451
babymonkey1111 <babymonkey111@outlook.com>
2022-09-30 12:13:41 -0300

    creating progress bar

--- client/src/components/Delivery.tsx
+++ client/src/components/Delivery.tsx
@@ -1,7 +1,19 @@
+import { useState } from 'react'
+
 const Delivery = () => {
+  const [state, setState] = useState({
+    id: '01GE7DKW4TBNQRRSS4SF93C4Y0',
+    budget: 50,
+    notes: 'Pick 2 burguers',
+    status: 'readay',
+  })
+
   return (
     <div>
-      <h1 className="text-3xl font-bold">Delivery</h1>
+      <h1 className="text-3xl font-bold dark:text-white">
+        Delivery {state.id}
+      </h1>
+      {state.status != 'ready' ? <div className='h-4 bg-green-600 progress rounded-sm'></div> : ''}
     </div>
   )
 }
