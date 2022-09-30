1fa2ad3d71be79a48bb368aab482431d7a99bf38
babymonkey1111 <babymonkey111@outlook.com>
2022-09-30 09:47:23 -0300

    deliver products

--- server/consumers.py
+++ server/consumers.py
@@ -16,10 +16,12 @@ def start_delivery(state, event):
         "status": "active"
     }
 
+
 def pickup_products(state, event):
     data = json.loads(event.data)
-    new_budget = state["budget"] - int(data['purchase_price']) * int(data['quantity'])
-    
+    new_budget = state["budget"] - \
+        int(data['purchase_price']) * int(data['quantity'])
+
     return state | {
         "budget": new_budget,
         "purchase_price": int(data['purchase_price']),
@@ -28,8 +30,23 @@ def pickup_products(state, event):
     }
 
 
+def deliver_products(state, event):
+    data = json.loads(event.data)
+    new_budget = state["budget"] + \
+        int(data['sell_price']) * int(data['quantity'])
+    new_quantity = state['quantity'] - int(data['quantity'])
+
+    return state | {
+        "budget": new_budget,
+        "sell_price": int(data['sell_price']),
+        "quantity": new_quantity,
+        "status": "completed"
+    }
+
+
 CONSUMERS = {
     "CREATE_DELIVERY": create_delivery,
     "START_DELIVERY": start_delivery,
-    "PICKUP_PRODUCTS": pickup_products 
+    "PICKUP_PRODUCTS": pickup_products,
+    "DELIVER_PRODUCTS": deliver_products
 }
