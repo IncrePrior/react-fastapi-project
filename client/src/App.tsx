376ee18d0fbe64b7b619efe432418ac63ee3ceb5
babymonkey1111 <babymonkey111@outlook.com>
2022-09-30 11:17:52 -0300

    create delivery setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,25 @@
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold">App</h1>
+    <div className="h-screen w-screen grid items-center justify-center bg-slate-100 dark:bg-slate-800">
+      <div className="flex flex-col items-center py-7 px-5 gap-3 rounded bg-white dark:bg-slate-100 shadow-lg">
+        <h1 className="text-2xl font-bold mb-2">Create Delivery</h1>
+        <input
+          type="number"
+          name="budget"
+          placeholder="Budget"
+          className="w-56 p-2 shadow rounded border-2 dark:border-0 outline-none transition duration-150 focus:shadow-md"
+        />
+        <textarea
+          name="notes"
+          placeholder="Notes"
+          className="w-56 h-24 p-2 shadow rounded border-2 dark:border-0 outline-none  transition duration-150 focus:shadow-md resize-none"
+        />
+        <button className="bg-emerald-500 text-white py-2 px-4 rounded shadow cursor-pointer transition duration-150 hover:brightness-90 hover:shadow-md active:brightness-75">
+          Submit
+        </button>
+      </div>
     </div>
   )
 }
+
 export default App
