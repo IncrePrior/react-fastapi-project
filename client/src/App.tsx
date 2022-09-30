a6a916a39e7a528b74bfb14f3b4e7d997119a3ce
babymonkey1111 <babymonkey111@outlook.com>
2022-09-30 11:47:22 -0300

    create delivery to its own component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import CreateDelivery from './components/CreateDelivery'
 import Delivery from './components/Delivery'
 
 const App = () => {
@@ -22,30 +23,7 @@ const App = () => {
 
   return (
     <div className="h-screen w-screen grid items-center justify-center bg-slate-100 dark:bg-slate-800">
-      {id === '' ? (
-        <form
-          className="flex flex-col items-center py-7 px-5 gap-3 rounded bg-white dark:bg-slate-100 shadow-lg"
-          onSubmit={handleSubmit}
-        >
-          <h1 className="text-2xl font-bold mb-2">Create Delivery</h1>
-          <input
-            type="number"
-            name="budget"
-            placeholder="Budget"
-            className="w-56 p-2 shadow rounded border-2 dark:border-0 outline-none transition duration-150 focus:shadow-md"
-          />
-          <textarea
-            name="notes"
-            placeholder="Notes"
-            className="w-56 h-24 p-2 shadow rounded border-2 dark:border-0 outline-none  transition duration-150 focus:shadow-md resize-none"
-          />
-          <button className="bg-emerald-500 text-white py-2 px-4 rounded shadow cursor-pointer transition duration-150 hover:brightness-90 hover:shadow-md active:brightness-75">
-            Submit
-          </button>
-        </form>
-      ) : (
-        <Delivery />
-      )}
+      {id === '' ? <CreateDelivery submit={handleSubmit} /> : <Delivery />}
     </div>
   )
 }
