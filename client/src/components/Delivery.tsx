0eb087e3724f381b8c8542f22548031a7d82b24a
babymonkey1111 <babymonkey111@outlook.com>
2022-10-03 10:47:41 -0300

    progress bar styled

--- client/src/components/Delivery.tsx
+++ client/src/components/Delivery.tsx
@@ -5,7 +5,7 @@ const Delivery = () => {
     id: '01GE7DKW4TBNQRRSS4SF93C4Y0',
     budget: 50,
     notes: 'Pick 2 burguers',
-    status: 'readay',
+    status: 'completed',
   })
 
   return (
@@ -13,9 +13,26 @@ const Delivery = () => {
       <h1 className="text-3xl font-bold dark:text-white">
         Delivery {state.id}
       </h1>
-      <div className="w-32 mx-auto rounded-sm overflow-hidden">
-        {state.status != 'ready' ? (
-          <div className=" h-4 bg-green-600 progress-bar rounded-sm"></div>
+      <div className="h-4 flex items-center mx-auto rounded-sm bg-slate-50">
+        {state.status !== 'ready' ? (
+          <div
+            className={
+              state.status === 'active'
+                ? 'h-4 bg-green-600 progress-bar-striped w-1/2'
+                : 'h-4 bg-green-600 w-1/2'
+            }
+          ></div>
+        ) : (
+          ''
+        )}
+        {state.status === 'collected' || state.status === 'completed' ? (
+          <div
+            className={
+              state.status === 'collected'
+                ? 'h-4 bg-green-600 progress-bar-striped w-1/2'
+                : 'h-4 bg-green-600 w-1/2'
+            }
+          ></div>
         ) : (
           ''
         )}
