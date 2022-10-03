5fdfcba8b999c5ca2ec7c29cf5666ab8ed1d1445
babymonkey1111 <babymonkey111@outlook.com>
2022-10-03 13:22:59 -0300

    pickup products single component

--- client/src/components/Delivery.tsx
+++ client/src/components/Delivery.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import BackHome from './BackHome'
 import IncreaseBudget from './IncreaseBudget'
+import PickupProducts from './PickupProducts'
 import StartDelivery from './StartDelivery'
 
 interface IDeliveryProps {
@@ -87,27 +88,7 @@ const Delivery: React.FC<IDeliveryProps> = (props) => {
         <IncreaseBudget submit={handleSubmit} />
 
         {/* Pickup Products Event */}
-        <form
-          onSubmit={(e) => handleSubmit(e, 'PICKUP_PRODUCTS')}
-          className="h-fit flex flex-col items-center py-7 px-5 gap-3 rounded bg-white dark:bg-slate-100 shadow-lg"
-        >
-          <h1 className="text-2xl font-bold mb-2">Pickup Products</h1>
-          <input
-            type="number"
-            name="purchase_price"
-            placeholder="Purchase Price"
-            className="w-56 p-2 shadow rounded border-2 dark:border-0 outline-none transition duration-150 focus:shadow-md"
-          />
-          <input
-            type="number"
-            name="quantity"
-            placeholder="Quantity"
-            className="w-56 p-2 shadow rounded border-2 dark:border-0 outline-none transition duration-150 focus:shadow-md"
-          />
-          <button className="bg-emerald-500 text-white py-2 px-4 rounded shadow cursor-pointer transition duration-150 hover:brightness-90 hover:shadow-md active:brightness-75">
-            Submit
-          </button>
-        </form>
+        <PickupProducts submit={handleSubmit} />
 
         {/* Deliver Products Event */}
         <form
---
+++ client/src/components/PickupProducts.tsx
@@ -0,0 +1,31 @@
+interface IPickupProducts {
+  submit: (e: any, type: string) => void
+}
+
+const PickupProducts: React.FC<IPickupProducts> = ({ submit }) => {
+  return (
+    <form
+      onSubmit={(e) => submit(e, 'PICKUP_PRODUCTS')}
+      className="h-fit flex flex-col items-center py-7 px-5 gap-3 rounded bg-white dark:bg-slate-100 shadow-lg"
+    >
+      <h1 className="text-2xl font-bold mb-2">Pickup Products</h1>
+      <input
+        type="number"
+        name="purchase_price"
+        placeholder="Purchase Price"
+        className="w-56 p-2 shadow rounded border-2 dark:border-0 outline-none transition duration-150 focus:shadow-md"
+      />
+      <input
+        type="number"
+        name="quantity"
+        placeholder="Quantity"
+        className="w-56 p-2 shadow rounded border-2 dark:border-0 outline-none transition duration-150 focus:shadow-md"
+      />
+      <button className="bg-emerald-500 text-white py-2 px-4 rounded shadow cursor-pointer transition duration-150 hover:brightness-90 hover:shadow-md active:brightness-75">
+        Submit
+      </button>
+    </form>
+  )
+}
+
+export default PickupProducts
