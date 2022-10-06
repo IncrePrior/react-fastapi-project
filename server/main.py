a91f9349856b4a88528bed65ca2ab56851ebd876
babymonkey1111 <babymonkey111@outlook.com>
2022-10-06 19:36:01 -0300

    removed unused import

--- server/main.py
+++ server/main.py
@@ -1,7 +1,7 @@
 import json
 import consumers
 import os
-from fastapi import FastAPI, Request, Response
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from redis_om import get_redis_connection, HashModel
 
